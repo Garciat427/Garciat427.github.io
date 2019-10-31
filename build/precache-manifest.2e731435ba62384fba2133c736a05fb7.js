08cf80c4e8c08ce34bf1b2ee373da5353d31bb64
Troy Garcia <tg1212@outlook.com>
2019-10-31 14:43:44 -0400

    Pushing Deployable gh-page

---
+++ build/precache-manifest.2e731435ba62384fba2133c736a05fb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac1dcdce93fae1ef0474932b01cc8e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca6fdc8a8fa6ae387103",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1fd91c13683230d3fccd",
+    "url": "/static/js/2.d59ee381.chunk.js"
+  },
+  {
+    "revision": "ca6fdc8a8fa6ae387103",
+    "url": "/static/js/main.c27799e0.chunk.js"
+  },
+  {
+    "revision": "6d60b4ae05f839f97de9",
+    "url": "/static/js/runtime-main.b9740393.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
