bf802d274a8b842bfcb226901facebe9e165d303
sheenagarcia <tg1212@outlook.com>
2019-11-01 16:54:08 -0400

    Updates

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+/* $(document).ready(function() {
+     $('#logo').animate({opacity: 1}, 2000, function(){
+          $('#slogan1').animate({opacity: 1}, 2000, function(){
+               $('#slogan2').animate({opacity: 1}, 2000, function(){
+          
+               })
+          })
+     });
+ }); */
---
+++ precache-manifest.d16e3d793855d3b1a9d35d2a5ef43269.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464c06604849c34ffb9d49ac88ed3915",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb829ce03cb81e39f069",
+    "url": "/static/js/2.887990f1.chunk.js"
+  },
+  {
+    "revision": "71185c07a056fabdb502",
+    "url": "/static/js/main.3987de5f.chunk.js"
+  },
+  {
+    "revision": "f6449039539f93adaf4e",
+    "url": "/static/js/runtime-main.7cb9914e.js"
+  }
+]);
