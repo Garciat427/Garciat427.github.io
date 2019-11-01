6ee01412856c23e74d3e8c6a88eddd7dacdebac5
sheenagarcia <tg1212@outlook.com>
2019-11-01 10:20:02 -0400

    Added 1 item to portfolio

--- src/components/pages/ProjectSpotlight.js
+++ src/components/pages/ProjectSpotlight.js
@@ -7,12 +7,17 @@ function ProjectSpotlight() {
          <div className="col s12 m6">
             <div className="card">
                <div className="card-image">
-                  <img src="images/sample-1.jpg"></img>
-                  <span className="card-title">Card Title</span>
-                  <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></a>
+               <img src="assets/images/DipmatMed-Front-Page.png" alt="Unsplashed background img 1"></img>
+                  <span className="card-title">Med 2.0 Symptom and Medical Trend Tracker</span>
+                  <a href="https://github.com/Garciat427/Med-2.0" className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></a>
                </div>
                <div className="card-content">
-                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                  <p>
+                     Current Status: In Development (Not Deployed)<br></br>
+                     Med 2.0 is an application that checks users symptoms, provides a diagnosis, and analyzes common illness trends within the area. <br></br>
+                     Technologies used: ReactJS, NodeJS, ExpressJS, MySQL, SequelizeORM, MateralizeCSS
+                  </p>
+                  <p><i class="fab fa-react techIcon"></i> <i class="fab fa-node techIcon"></i> <i class="fas fa-server techIcon"></i> <i class="fas fa-database techIcon"></i></p>
                </div>
             </div>
          </div>
