6243ec617776ba5a61db1a1d3e4eebf8cc1952b2
sheenagarcia <tg1212@outlook.com>
2019-11-03 01:32:26 -0400

    Changed Styling on Floating Button

--- src/components/pages/FloatingActionBtn.js
+++ src/components/pages/FloatingActionBtn.js
@@ -3,15 +3,40 @@ import React from "react";
 function FloatingActionBtn() {
    return (
       <div className="fixed-action-btn">
-         <a className="btn-floating btn-large blue-grey darken-4">
-            <i class="fas fa-link"></i>
+         <a className="btn-floating btn-large blue-grey darken-4 hoverable">
+            <i class="fas fa-link link-Action-Btn"></i>
          </a>
          <ul>
-            <li><a href="https://github.com/Garciat427" className="btn-floating blue-grey darken-3 hoverable link-Action-Btn"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/garciat427" className="btn-floating blue-grey darken-3 hoverable link-Action-Btn"><i class="fab fa-linkedin-in"></i></a></li>
-            <li><a href="mailto:tg1212@outlook.com" className="btn-floating blue-grey darken-3 hoverable link-Action-Btn"><i class="fas fa-envelope"></i></a></li>
-            <li><a href="https://twitter.com/Garciat427" className="btn-floating blue-grey darken-3 hoverable link-Action-Btn"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://www.instagram.com/garciat427/" className="btn-floating blue-grey darken-3 hoverable link-Action-Btn"><i class="fab fa-instagram"></i></a></li>
+            <li>
+               <a href="https://github.com/Garciat427" 
+               className="btn-floating blue-grey darken-3 hoverable">
+                  <i class="fab fa-github link-Action-Btn"></i>
+               </a>
+            </li>
+            <li>
+               <a href="https://www.linkedin.com/in/garciat427" 
+               className="btn-floating blue-grey darken-3 hoverable">
+                  <i class="fab fa-linkedin-in link-Action-Btn"></i>
+               </a>
+            </li>
+            <li>
+               <a href="mailto:tg1212@outlook.com" 
+               className="btn-floating blue-grey darken-3 hoverable">
+                  <i class="fas fa-envelope link-Action-Btn"></i>
+               </a>
+            </li>
+            <li>
+               <a href="https://twitter.com/Garciat427" 
+               className="btn-floating blue-grey darken-3 hoverable">
+                  <i class="fab fa-twitter link-Action-Btn"></i>
+               </a>
+            </li>
+            <li>
+               <a href="https://www.instagram.com/garciat427/" 
+               className="btn-floating blue-grey darken-3 hoverable">
+                  <i class="fab fa-instagram link-Action-Btn"></i>
+               </a>
+            </li>
          </ul>
       </div>
    );
