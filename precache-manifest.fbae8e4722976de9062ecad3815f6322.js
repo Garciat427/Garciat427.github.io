8dc006abfde3ad97a2d7e9fed862dd2273db9cf6
sheenagarcia <tg1212@outlook.com>
2019-11-03 04:13:23 -0500

    Updates

--- precache-manifest.fbae8e4722976de9062ecad3815f6322.js
+++ precache-manifest.fbae8e4722976de9062ecad3815f6322.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48333bb5145d7dcc89b5ed932377c134",
+    "revision": "f9f06010ee91f745eb6e548f69796db3",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.887990f1.chunk.js"
   },
   {
-    "revision": "263864dac813ff1650e0",
-    "url": "/static/js/main.a6049e73.chunk.js"
+    "revision": "f15c8a372928e6038bfc",
+    "url": "/static/js/main.2415952c.chunk.js"
   },
   {
     "revision": "f6449039539f93adaf4e",
