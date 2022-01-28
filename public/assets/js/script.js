8ec56b20cce1bcc88f0aa5bdb82c66ea335db0f3
Troy Garcia <tg1212@outlook.com>
2022-01-27 23:15:29 -0800

    Clearing old project

--- public/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-/* $(document).ready(function() {
-     $('#logo').animate({opacity: 1}, 2000, function(){
-          $('#slogan1').animate({opacity: 1}, 2000, function(){
-               $('#slogan2').animate({opacity: 1}, 2000, function(){
-          
-               })
-          })
-     });
- }); */
